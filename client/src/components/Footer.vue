4e66d647bec2557f133046fdabbb8fc46e8c19ea
Tim <serazutdinov.tim@gmail.com>
2021-04-15 01:58:42 +0300

    Colors

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="w-full ">
+        <slot></slot>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer', 
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,22 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      warm: colors.warmGray,
+      yellow: colors.amber,
+      lightBlue: colors.lightBlue,
+      gray: colors.blueGray,
+      green: colors.green,
+      txt: {
+        black: "#44403C",
+        white: "#FAFAF9"
+      },
+      rose: colors.rose,
+
+    },
     extend: {},
   },
   variants: {
